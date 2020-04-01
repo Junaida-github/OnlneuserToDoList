8fd204053f80e4bbbb61f425da78386768d6c05e
Junaida-github <junufaizu@gmail.com>
2020-04-01 07:57:55 +0530

    second commit

---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineUsrToDoLst.Models
+{
+    public class RegistrationModel
+    {
+            [Required]
+            public string DisplayName { get; set; }
+            [Required]
+            public string Password { get; set; }
+            [Required]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+    }
+
+    public class Login
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class ToDos
+    {
+        public int ToDoId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public System.DateTime DueDate { get; set; }
+    }
+}
---
+++ Models/UserIdentityDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineUsrToDoLst.Models
+{
+    public class UserIdentityDbContext: IdentityDbContext<UserIdentityUser>
+    {
+        public UserIdentityDbContext(): base("connectionstring")
+        {
+
+        }
+    }
+
+    public class UserIdentityUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserIdentityRole : IdentityRole
+    {
+        public UserIdentityRole()
+        {
+
+        }
+
+        public UserIdentityRole(string roleName, string description)
+            : base(roleName)
+        {
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
